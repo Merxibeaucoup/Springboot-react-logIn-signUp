9f2e6dc1c5e9b72280c64237e9471e7467ddf4c4
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-09 22:59:47 -0500

    logout fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import newRequest from "../utils/newRequest";
 
 const Home = () => {
-  return <div>Home</div>;
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await newRequest.post("/auth/logout");
+      localStorage.setItem("currentUser", null);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+      <span>{currentUser?.email}</span>
+    </div>
+  );
 };
 
 export default Home;
